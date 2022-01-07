1bb68fec864d86230e3e7a07fd51665e1eb22e35
Killian <thevenin.killian@gmail.com>
2022-01-07 18:16:59 +0100

    update

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,9 @@
 <template>
   <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+    <header>Header</header>
+
     <router-view />
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
-      [Default Layout]
-    </div>
+
+    <footer>Footer</footer>
   </main>
 </template>
