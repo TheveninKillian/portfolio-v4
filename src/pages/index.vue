0f712f787d60b6494376ed02f4d160b9b74a01bb
Killian <thevenin.killian@gmail.com>
2022-01-07 18:16:42 +0100

    update: config unoss + test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,13 +3,17 @@
 <template>
   <div>
     <div i-carbon-campsite text-4xl inline-block />
-    <p>
+    <h1 class="uno-title">
       <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
         Vitesse Lite
       </a>
-    </p>
-    <p>
+    </h1>
+    <p class="uno-text">
       <em text-sm op75>Opinionated Vite Starter Template</em>
     </p>
+
+    <button class="uno-btn">
+      Test
+    </button>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -39,8 +39,9 @@ export default defineConfig({
 
     Unocss({
       shortcuts: [
-        ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-        ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+        ['uno-btn', 'px-10px py-5px transition duration-300'],
+        ['uno-text', 'mb-30px lg:mb-60px center'],
+        ['uno-title', 'mb-20px lg:mb-25px center'],
       ],
       presets: [
         presetUno(),
