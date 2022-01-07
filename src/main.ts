2223b3b7e97ad3af528e22a8528a0f1cd12a5a19
Killian <thevenin.killian@gmail.com>
2022-01-07 16:17:17 +0100

    css to scss

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
-import './styles/main.css'
+import './styles/main.scss'
 import 'uno.css'
 
 const app = createApp(App)
