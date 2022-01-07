d35a5b8be96b36f4f3e3633316aaa78685cc73cd
Killian <thevenin.killian@gmail.com>
2022-01-07 16:01:11 +0100

    install vite-plugin-vue-layouts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-// register vue composition api globally
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import routes from 'virtual:generated-pages'
+import generatedRoutes from 'virtual:generated-pages'
+import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
@@ -9,9 +9,9 @@ import './styles/main.css'
 import 'uno.css'
 
 const app = createApp(App)
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
+
+const routes = setupLayouts(generatedRoutes)
+const router = createRouter({ history: createWebHistory(), routes })
 app.use(router)
+
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { presetAttributify, presetIcons, presetUno } from 'unocss'
@@ -18,10 +19,10 @@ export default defineConfig({
   plugins: [
     Vue(),
 
-    // https://github.com/hannoeru/vite-plugin-pages
     Pages(),
 
-    // https://github.com/antfu/unplugin-auto-import
+    Layouts(),
+
     AutoImport({
       imports: [
         'vue',
@@ -32,12 +33,10 @@ export default defineConfig({
       dts: true,
     }),
 
-    // https://github.com/antfu/vite-plugin-components
     Components({
       dts: true,
     }),
 
-    // https://github.com/antfu/unocss
     Unocss({
       shortcuts: [
         ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
