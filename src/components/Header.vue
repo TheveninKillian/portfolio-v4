91024f9607b2ed7c95001c86c6ef0b19103e0e44
Killian <thevenin.killian@gmail.com>
2022-01-14 17:52:20 +0100

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,4 @@
+/* eslint-disable vue/html-self-closing */
 <script setup lang="ts">
 import { useWindowSize } from '@vueuse/core'
 import { Icon } from '@iconify/vue'
@@ -5,6 +6,7 @@ import { Icon } from '@iconify/vue'
 const { width } = useWindowSize()
 
 const navActive = ref(false)
+const borderSelected = ref('home')
 </script>
 
 <template>
@@ -12,19 +14,23 @@ const navActive = ref(false)
     <p>Killian Thevenin</p>
 
     <div class="naviguated">
-      <div v-if="width <= 992">
+      <div v-if="width <= 1200">
         <Icon icon="ci:hamburger" @click="navActive = !navActive" />
       </div>
 
-      <nav :class="{ active: navActive }">
+      <nav :class="{ active: navActive }" @click="navActive = !navActive">
         <ul>
-          <li>
-            Accueil
-          </li>
+          <router-link to="/">
+            <li :class="{ activeBorder: borderSelected === 'home' }" @click="borderSelected = 'home'">
+              Accueil
+            </li>
+          </router-link>
 
-          <li>
-            Portfolio
-          </li>
+          <router-link to="/portfolio">
+            <li :class="{ activeBorder: borderSelected === 'portfolio' }" @click="borderSelected = 'portfolio'">
+              Portfolio
+            </li>
+          </router-link>
         </ul>
       </nav>
     </div>
@@ -41,6 +47,7 @@ header{
 
   display: flex;
   justify-content: space-between;
+  align-items: center;
 
   padding-top: $marge-all-min;
   padding-bottom: $marge-all-min;
@@ -77,19 +84,92 @@ header{
   nav{
     position: fixed;
     top: 0;
-    left: -70%;
+    left: -100%;
 
     height: 100vh;
-    width: 70%;
+    width: 100%;
 
-    background-color: $color-background;
+    transition: left .5s;
 
-    transition: left .3s;
+    ul{
+      width: 70%;
+      height: 100vh;
+
+      background-color: $color-background;
+
+      @include breakpoint(sm){
+        width: 30%;
+      }
+
+      @include breakpoint(lg){
+        display: flex;
+        align-items: center;
+
+        height: auto;
+        width: auto;
+      }
+
+      a{
+        color: $color-text;
+        text-decoration: none;
+      }
+
+      .router-link-active{
+        color: $color-top;
+      }
+
+      li{
+        padding: 20px 0;
+
+        font-size: $h3-mobile;
+        text-align: center;
+
+        @include breakpoint(lg){
+          margin-left: 15px;
+          padding: 10px;
+        }
+      }
+
+      .activeBorder{
+        position: relative;
+
+        border: 2px solid $color-top;
+
+        &::before, &::after{
+          content: '';
+          position: absolute;
+          left: 50%;
+
+          height: 2px;
+          width: 50%;
+
+          transform: translateX(-50%);
+
+          background-color: $color-background;
+        }
+
+        &::before{
+          top: -2px;
+        }
+
+        &::after{
+          bottom: -2px;
+        }
+
+        @include breakpoint(lg){
+          border: none;
+
+          text-decoration: underline;
+
+          &::before, &::after{
+            display: none;
+          }
+        }
+      }
+    }
 
     @include breakpoint(sm){
       left: -30%;
-
-      width: 30%;
     }
 
     @include breakpoint(lg){
