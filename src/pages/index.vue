6bbcd6e145877b3c7a254f2484cc6c0e55488b3b
Killian <thevenin.killian@gmail.com>
2022-01-14 15:59:53 +0100

    delete class uno

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,16 +3,16 @@
 <template>
   <div>
     <div i-carbon-campsite text-4xl inline-block />
-    <h1 class="uno-title">
+    <h1>
       <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
         Vitesse Lite
       </a>
     </h1>
-    <p class="uno-text">
+    <p>
       <em text-sm op75>Opinionated Vite Starter Template</em>
     </p>
 
-    <button class="uno-btn">
+    <button>
       Test
     </button>
   </div>
