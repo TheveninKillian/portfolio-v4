c5948f1b3f97769bb4696569815632fa8f30eaed
Killian <thevenin.killian@gmail.com>
2022-01-14 16:39:30 +0100

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
 import { useWindowSize } from '@vueuse/core'
+import { Icon } from '@iconify/vue'
 
 const { width } = useWindowSize()
+
+const navActive = ref(false)
 </script>
 
 <template>
   <header>
     <p>Killian Thevenin</p>
 
-    <div>
+    <div class="naviguated">
       <div v-if="width <= 992">
-        Button
+        <Icon icon="ci:hamburger" @click="navActive = !navActive" />
       </div>
 
-      <nav>
+      <nav :class="{ active: navActive }">
         <ul>
           <li>
             Accueil
@@ -28,8 +31,9 @@ const { width } = useWindowSize()
   </header>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import '../styles/variables.scss';
+@import '../styles/mixins.scss';
 
 header{
   position: sticky;
@@ -42,10 +46,62 @@ header{
   padding-bottom: $marge-all-min;
 
   background-color: $color-background;
+
+  @include breakpoint(lg){
+    padding-top: $marge-all-max;
+    padding-bottom: $marge-all-max;
+  }
+
+  p{
+    font-size: $h3-mobile;
+    font-weight: bold;
+
+    @include breakpoint(sm){
+      font-size: $h3-tablet;
+    }
+    @include breakpoint(lg){
+      font-size: $h3-desktop;
+    }
+  }
 }
 
-header p{
-  font-size: $h3-mobile;
-  font-weight: bold;
+.naviguated{
+  svg{
+    font-size: $h2-mobile;
+
+    @include breakpoint(sm){
+      font-size: $h2-tablet;
+    }
+  }
+
+  nav{
+    position: fixed;
+    top: 0;
+    left: -70%;
+
+    height: 100vh;
+    width: 70%;
+
+    background-color: $color-background;
+
+    transition: left .3s;
+
+    @include breakpoint(sm){
+      left: -30%;
+
+      width: 30%;
+    }
+
+    @include breakpoint(lg){
+      position: inherit;
+
+      height: auto;
+      width: auto;
+    }
+
+    &.active{
+      left: 0;
+    }
+  }
 }
 </style>
