f5440530225bb24eb09adc9bb6eab2502b11dc89
Killian <thevenin.killian@gmail.com>
2022-01-15 16:01:33 +0100

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useWindowSize } from '@vueuse/core'
+import { Icon } from '@iconify/vue'
+
+const { width } = useWindowSize()
+</script>
 
 <template>
-  <div>
-    <div i-carbon-campsite text-4xl inline-block />
+  <section>
     <h1>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
+      Développeur front-end,<br v-if="width < 500">
+      <img class="letter-img" src="https://res.cloudinary.com/dbfzyht7k/image/upload/v1636387765/gmn4cvujyng6ngm5oeei.png" alt="logo vue js">ue.js
     </h1>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
 
-    <button>
-      Test
-    </button>
-  </div>
+    <button>Contactez moi <Icon icon="akar-icons:arrow-down" /></button>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+@import '../styles/mixins.scss';
+
+h1{
+  margin-bottom: 15px;
+
+  @include breakpoint(md){
+    margin-bottom: 25px;
+  }
+}
+
+.letter-img {
+  display: inline-block;
+
+  width: 30px;
+
+  @include breakpoint(sm){
+    width: 50px;
+  }
+}
+
+button {
+  svg{
+    margin-left: 5px;
+  }
+}
+</style>
