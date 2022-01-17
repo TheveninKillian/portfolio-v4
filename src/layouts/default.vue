70f2c663c653980b765ade5c9bd9c757260aefce
Killian <thevenin.killian@gmail.com>
2022-01-17 16:48:56 +0100

    update footer + style

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -4,6 +4,30 @@
 
     <router-view />
 
-    <footer>Footer</footer>
+    <hr>
+
+    <footer>
+      <p>Design et développement : Killian</p>
+    </footer>
   </main>
 </template>
+
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+@import '../styles/mixins.scss';
+
+footer{
+  padding-top: $marge-section-min;
+  padding-bottom: $marge-section-min;
+
+  @include breakpoint(lg){
+    padding-top: $marge-section-max;
+    padding-bottom: $marge-section-max;
+  }
+
+  p{
+    color: $color-top;
+    text-align: center;
+  }
+}
+</style>
